5e790df4eadf53f163cddb15f416a8cd7e227fd4
raulbarranqueroguerrero <barranqueroguerrero@gmail.com>
2021-02-19 00:57:01 +0100

    Replace porfolio imgs and updatecontact message

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -26,7 +26,7 @@ class Contact extends Component {
 
             <div className="ten columns">
 
-                  <p className="lead">{message}</p>
+                  <p style={{letterSpacing: '.5rem', wordSpacing: '.5rem'}}className="lead">{message}</p>
 
             </div>
 
